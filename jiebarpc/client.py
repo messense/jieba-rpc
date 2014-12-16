da3747f91f4af932f0fb4199b15655207ef2710e
messense <messense@icloud.com>
2014-12-16 17:37:43 +0800

    Expose RPC methods on RPC client

--- jiebarpc/client.py
+++ jiebarpc/client.py
@@ -9,12 +9,56 @@ class JiebaRPCClient(msgpackrpc.Client):
         address = msgpackrpc.Address(host, port)
         super(JiebaRPCClient, self).__init__(address, *args, **kwargs)
 
+    def cut(self, sentence, cut_all=False, HMM=True):
+        return self.call(
+            'cut',
+            sentence,
+            cut_all,
+            HMM
+        )
+
+    def cut_for_search(self, sentence):
+        return self.call(
+            'cut_for_search',
+            sentence
+        )
+
+    def extract_tags(self, sentence, topK=20, withWeight=False):
+        return self.call(
+            'extract_tags',
+            sentence,
+            topK,
+            withWeight
+        )
+
+    def textrank(self, sentence, topK=10, withWeight=False):
+        return self.call(
+            'textrank',
+            sentence,
+            topK,
+            withWeight
+        )
+
+    def posseg(self, sentence, HMM=True):
+        return self.call(
+            'posseg',
+            sentence,
+            HMM
+        )
+
+    def tokenize(self, sentence, mode='default', HMM=True):
+        return self.call(
+            'tokenize',
+            sentence,
+            mode,
+            HMM
+        )
 
 if __name__ == '__main__':
     client = JiebaRPCClient('localhost', 4444)
-    print(client.call('cut', '测试分词'))
-    print(client.call('cut_for_search', '测试分词'))
-    print(client.call('extract_tags', '测试分词'))
-    print(client.call('textrank', '测试分词'))
-    print(client.call('tokenize', '测试分词'))
-    print(client.call('posseg', '测试分词'))
+    print(client.cut('测试分词'))
+    print(client.cut_for_search('测试分词'))
+    print(client.extract_tags('测试分词'))
+    print(client.textrank('测试分词'))
+    print(client.tokenize('测试分词'))
+    print(client.posseg('测试分词'))
